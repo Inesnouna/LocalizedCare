0e1be66809669af292874e8cb9826d61d8cf79a0
Inesnouna <inesaoues0@gmail.com>
2018-05-04 18:45:39 +0100

    comitting source code

--- app/src/main/java/com/example/ines/localizedcare/Main4Activity.java
+++ app/src/main/java/com/example/ines/localizedcare/Main4Activity.java
@@ -3,11 +3,11 @@ package com.example.ines.localizedcare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TypeSoins extends AppCompatActivity {
+public class Main4Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_type_soins);
+        setContentView(R.layout.activity_main4);
     }
 }
--- app/src/main/java/com/example/ines/localizedcare/Main7Activity.java
+++ app/src/main/java/com/example/ines/localizedcare/Main7Activity.java
@@ -3,11 +3,11 @@ package com.example.ines.localizedcare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class EspaceMedecin extends AppCompatActivity {
+public class Main7Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_espace_medecin);
+        setContentView(R.layout.activity_main7);
     }
 }
